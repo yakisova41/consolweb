37494cbc433c430f713f2f6aa7cbe5d3a02b225e
yakisova41 <yakisoba5emon@gmail.com>
2024-03-07 19:07:21 +0000

    📝 update demo

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,19 @@ new ConsolWeb({ bindNative: true, setGlobalThis: true });
 window.consolweb.log("Console Web");
 
 const progress = window.consolweb.progress(0, 20, {
-  beforeText: "Nice Progress",
+  beforeText: "Progress",
   showPercent: true,
   showLength: true,
   loop: true,
-  alwaysBelow: true,
 });
 let i = 1;
-let isUp = true;
-const pp = setInterval(() => {
+const interval = setInterval(() => {
   progress.update(i);
 
-  if (i >= 20) {
-    isUp = false;
+  if (i === 20) {
+    clearInterval(interval);
+    progress.clear();
   }
 
-  if (i === 0) {
-    isUp = true;
-  }
-
-  if (isUp) {
-    i++;
-  } else {
-    i--;
-  }
+  i++;
 }, 500);
-
-const s = setInterval(() => {
-  console.log(Math.random());
-}, 100);
-
-setTimeout(() => {
-  clearInterval(pp);
-  progress.clear();
-  clearInterval(s);
-}, 5000);
